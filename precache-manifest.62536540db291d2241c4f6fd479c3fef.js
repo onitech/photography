70dee61725920d27f822ed1e43b439513cc84bfc
onitech <mc.busante@gmail.com>
2020-04-20 01:52:03 +0800

    Updates

---
+++ precache-manifest.62536540db291d2241c4f6fd479c3fef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede439396e44d5f7de35e029328131d1",
+    "url": "/photography/index.html"
+  },
+  {
+    "revision": "488fc22e2bd595c48f38",
+    "url": "/photography/static/css/main.ad53b380.chunk.css"
+  },
+  {
+    "revision": "2b420fe057cbf1637612",
+    "url": "/photography/static/js/2.b0634755.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/photography/static/js/2.b0634755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488fc22e2bd595c48f38",
+    "url": "/photography/static/js/main.c467818e.chunk.js"
+  },
+  {
+    "revision": "664bba4f3ccb449d2d0a",
+    "url": "/photography/static/js/runtime-main.9ee35b00.js"
+  },
+  {
+    "revision": "9133f6c2ebfd2a31bac3d178aeecdaba",
+    "url": "/photography/static/media/bg1.9133f6c2.jpg"
+  },
+  {
+    "revision": "c0070d2d62f3a7b92cf463fe2a8f185a",
+    "url": "/photography/static/media/bg3.c0070d2d.jpg"
+  }
+]);
